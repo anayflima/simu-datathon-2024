00a1e9a024d497820cf95fa47a2c75b15fecd3c1
Ana Lima <ayf.lima@usp.br>
2024-06-08 13:37:42 -0300

    Adiciona configuracoes para deploy do mapa em fly.io

--- mapa_interativo/app.py
+++ mapa_interativo/app.py
@@ -5,26 +5,20 @@ from bokeh.application import Application
 from bokeh.application.handlers.function import FunctionHandler
 from modulos_mapa_interativo import plot_mapa_interativo
 
-current_path = os.getcwd()
+pasta_dados = './dados/'
 
-if 'mapa_interativo' in current_path:
-    pasta_dados = '../dados/'
-else:
-    pasta_dados = './dados/'
+df_agrupado_merge = pd.read_csv(os.path.join(pasta_dados, 'merge_agrupamento_municipio_ano.csv'))
 
-df_municipios = pd.read_csv(os.path.join(pasta_dados, 'simu_carteira_municipios.csv'))
-df_agrupado = pd.read_csv(os.path.join(pasta_dados, 'tratados/agrupamento_empreend_acidentes_por_municipio_e_ano_sem_nulos.csv'))
-
-df_agrupado_merge = df_agrupado.merge(df_municipios, on='Código IBGE', how='left')
 
 def modify_doc(doc):
     plot = plot_mapa_interativo(df_agrupado_merge)
     
     doc.add_root(plot.get_root(doc))
+    doc.title = 'Mapa Empreendimentos vs Acidentes'
 
 apps = {'/': Application(FunctionHandler(modify_doc))}
 
-server = Server(apps, port=5000)
+server = Server(apps, port=8080)
 server.start()
 
 if __name__ == '__main__':
